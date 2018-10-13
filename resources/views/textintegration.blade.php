e63f1e1534e670436a5e9e670ef2275bbc79f7ae
Julie Thome <Harry_Thome@Julies-MacBook-Pro.local>
2018-10-13 00:08:50 -0500

    dumb

---
+++ resources/views/textintegration.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card_margin">
+  <div class="card text-center" >
+  <div class="card-body">
+    <div class="page_title">
+      <h5 class="card-title">Talk with an Employee</h5>
+      <div class="card_bubble">
+    <p class="text_message">Hello Alex</p>
+
+    <div class="container_fluid">
+
+      <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Type a Message Here" >
+          <a href="#" class="btn btn-primary">➞</a>
+      </div>
+    </div>
+    </div>
+</div>
+
+@endsection
