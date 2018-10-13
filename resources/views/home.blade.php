de24c28ee73fadbeabb34f6601da586ee3d346fb
Alex Wasinger <25324676+awasinger@users.noreply.github.com>
2018-10-13 02:37:47 -0500

    home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <h4 class="home-margin-bottom">Jobs You Have Applied for</h4>
+            <div class="col-md-8">
+                <a href="#" class="home-card-link">
+                    <div class="card home-card">
+                        <div class="card-header">Job</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="card-body">
+                            This is a job desc
                         </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
-</div>
 @endsection
