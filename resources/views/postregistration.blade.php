1bf5eb2895e4c06acab6d9871c7690c9c3f13dda
Ian Crossey <iancrossey@Ians-MacBook-Pro.local>
2018-10-13 10:54:25 -0500

    Some Require stuff

--- resources/views/postregistration.blade.php
+++ resources/views/postregistration.blade.php
@@ -4,12 +4,12 @@
 <div class="card card_margin">
   <div class="card-body">
     <h4 class="card-title">Tell us about yourself!</h4>
-    
+
     <p class="card-text">
       <form id="UserInfo" action="#" method="post">
         <div class="form-group">
           <label for="sel1">Sex:</label>
-          <select class="form-control" id="sel1">
+          <select required class="form-control" id="sel1">
             <option value="select">Select One</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
@@ -21,18 +21,18 @@
           <label for="example-date-input" >Birthday:</label>
           <br>
           <div class="form-group">
-            <input class="form-control" type="date" id="bday">
+            <input class="form-control" type="date" id="bday" required>
           </div>
         </div>
 
         <div class="form-group">
           <label for="exampleInputCountry1">Country of Birth:</label>
-          <input type="text" class="form-control" id="exampleInputCountry1 CoB" placeholder="Country of Birth">
+          <input type="text" class="form-control" id="exampleInputCountry1 CoB" placeholder="Country of Birth" required>
         </div>
 
         <div class="form-group">
           <label for="exampleInputLanguage1">Primary Language:</label>
-          <input type="text" class="form-control" id="exampleInputLanguage1 PriLang" placeholder="Primary Language">
+          <input type="text" class="form-control" id="exampleInputLanguage1 PriLang" placeholder="Primary Language" required>
         </div>
 
         <div class="form-group">
